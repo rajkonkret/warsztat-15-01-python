6485a2fa1fa7aa62f38c48f3019e2b518976be7a
radek <rajkonkret660@gmail.com>
2024-01-16 13:31:26 +0100

    2 - add fun14

---
+++ 2.10 fun14-gen4-zad.py
@@ -0,0 +1,5 @@
+# stworzenie raportu, przetworzenie danych
+# generator danych - za pomoca generatora wygenerowac dane
+# wykorzystac dekorator do pomiaru czasu operacji
+# stworz raport - zmierzyc czas
+
