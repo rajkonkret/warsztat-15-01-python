c930d28372c8e8cbb2572c100a3c60da41e10172
radek <rajkonkret660@gmail.com>
2024-01-16 13:53:50 +0100

    2 - add fun15

--- 2.10 fun14-gen4-zad.py
+++ 2.10 fun14-gen4-zad.py
@@ -2,4 +2,36 @@
 # generator danych - za pomoca generatora wygenerowac dane
 # wykorzystac dekorator do pomiaru czasu operacji
 # stworz raport - zmierzyc czas
+import time
 
+
+def measure_time(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        print(f"Czas wykonania funkcji {func.__name__}: {execution_time}")
+        return result
+
+    return wrapper
+
+
+def generator_danych(dane):
+    for element in dane:
+        yield element
+
+
+def przetworz_dane(dane):
+    return [element for element in dane if element % 2 == 0]
+
+
+@measure_time
+def stworz_raport(dane):
+    for element in generator_danych(dane):
+        print(f"Raport dla systemu {element}")
+
+
+dane = range(100_000)
+pr_dane = przetworz_dane(dane)
+stworz_raport(pr_dane)
