78c404fd5a080cba33897163f263ecedf0a7b151
radek <rajkonkret660@gmail.com>
2024-01-17 15:39:52 +0100

    3 - add baza1

---
+++ 3.18 - baza1.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+try:
+    sql_connection = sqlite3.connect('sqlite_python.db')
+    cursor = sql_connection.cursor()
+    print("Baza danych zostałą podłaczona")
+except sqlite3.Error as e:
+    print("Bład podczas podłączania bazy danych", e)
+finally:
+    if sql_connection:
+        sql_connection.close()
+        print("Baza danch została zamknieta")
+
