1e18ae4af1d46f4af8371695ca3727fe9daece72
radek <rajkonkret660@gmail.com>
2024-01-18 13:04:33 +0100

    4 - add api_nbp xml add pydantic

--- 4.10 - api_nbp_xml.py
+++ 4.10 - api_nbp_xml.py
@@ -1,5 +1,21 @@
 import xml.etree.ElementTree as ET
 import requests as re
+from pydantic import BaseModel
+from typing import List
+
+
+class CurrencyRate(BaseModel):
+    currency: str
+    code: str
+    mid: float
+
+
+class ExchangeRatesTables(BaseModel):
+    table: str
+    date: str
+    number: str
+    rates: List[CurrencyRate]
+
 
 url = 'http://api.nbp.pl/api/exchangerates/tables/A/?format=xml'
 
@@ -29,9 +45,14 @@ print(rates)
 
 # wyciagnac te pola i wypisac
 # waluta kod_waluty kurs
+currency_rates = []
 for rate in rates:
     currency = rate.find('Currency').text
     code = rate.find('Code').text
     mid = rate.find('Mid').text
     print(f"Currency {currency}, Code: {code}, Mid: {mid}")
+    currency_rates.append(CurrencyRate(currency=currency, code=code, mid=mid))
 # Currency euro, Code: EUR, Mid: 4.4016
+exchange_rate_table = ExchangeRatesTables(table=table_name, date=data, number=no, rates=currency_rates)
+print(exchange_rate_table.rates[5])
+print(exchange_rate_table.rates[9])
